76e13f3484088cb716acf30ed4e77b3fb07d16b5
Filip <filipbart1@gmail.com>
2018-08-16 16:14:56 +0200

    Moving FrontSide.js and additional files to new directory

---
+++ src/FrontSide/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import FrontSideView from "./FrontSideView";
+import moment from "moment";
+import { getWeatherForLocation } from "../api";
+import cities from "../cities.json";
+
+class FrontSide extends Component {
+
+    state = {currentWeather: null};
+
+    componentDidMount(){
+        getWeatherForLocation(cities[0]).then(weather => {
+            this.setState({currentWeather: weather.currently});
+        });
+    }
+
+    render(){
+        if(!this.state.currentWeather){
+            return null;
+        }
+        
+        const {
+            icon,
+            temperature,
+            apparentTemperature,
+            summary
+        } = this.state.currentWeather;
+
+        return(
+            <FrontSideView
+                date={moment()}
+                icon={icon}
+                temperature={temperature}
+                apparentTemperature={apparentTemperature}
+                summary={summary}
+                currentCityName='Lublin'
+          />
+        )
+    }
+}
+
+export default FrontSide;
