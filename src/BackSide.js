a15dccaebd7900b314ab7841411c14d4418b3839
Filip <filipbart1@gmail.com>
2018-08-16 16:36:10 +0200

    Adding back side of weather app

---
+++ src/BackSide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CitiesList from "./CitiesList";
+
+export default ({ onClick }) => {
+    return(
+        <div>
+            <CitiesList />
+            <button onClick={onClick}>Flip back</button>
+        </div>
+    );
+}
---
+++ src/CitiesList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./list.css";
+
+export default () => {
+    return( 
+        <ul className="list">
+            <li className="list-item">
+                Lublin
+            </li>
+            <li className="list-item">
+                Warsaw
+            </li>
+        </ul>
+    );
+}
