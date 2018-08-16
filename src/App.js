bad3fbd3fd621cc80e29c6b51f904f6e38c4c840
Filip <filipbart1@gmail.com>
2018-08-16 16:16:27 +0200

    Moving FrontSide.js and additional files to new directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import FrontSide from "./FrontSide";
+import React, { Component } from "react";
+import FrontSide from './FrontSide/';
 import moment from "moment";
 import "./panel.css";
 
@@ -8,14 +8,7 @@ class App extends Component {
     return (
       <div className="panel">
         <div className="panel-front">
-          <FrontSide 
-            date={moment()}
-            icon='default'
-            temperature={19}
-            apparentTemperature={20}
-            summary="Sunny"
-            currentCityName='Lublin'
-          />
+          <FrontSide />
         </div>
         <div className="panel-back">
         
